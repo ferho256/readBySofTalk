85f3b121ac58c7c7b86e94cfe7b038e087d41634
ferho256 <ferho@protonmail.com>
2021-09-06 23:14:17 +0900

    First Commit

--- readBySofTalk.js
+++ readBySofTalk.js
@@ -1,1 +1,29 @@
+/*
+ * readBySoftalk
+ * 
+ * Meryで選択した文章をSoftalkで読み上げるマクロ
+ * 
+ */
 
+// SofTalk.exeのフルパスを指定
+const pathToSofTalk = "D://softalk//softalk.exe";
+
+// エディタ上で文字列が選択されていない場合は全文を選択する
+if (Document.Selection.IsEmpty){
+  document.selection.StartOfLine(false, mePosLogical);
+  document.selection.EndOfLine(true, mePosLogical);
+}
+
+let selectedText = Document.Selection.Text;
+
+/*
+ * SofTalk引数リファレンス
+ * SofTalk > ヘルプ > 引数指定
+ * 
+ * Meryのマクロでは`と${}を用いた文字列展開ができなかった(2021/09/05)
+ */
+let shell = new ActiveXObject('WScript.shell');
+const startCommand = pathToSofTalk + " /W:" + selectedText;
+const endCommand = pathToSofTalk + " /close2";
+shell.Run(startCommand);
+shell.Run(endCommand);
